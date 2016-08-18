b7c4a596dc851016880a3b5d8d4c64541d0ce801
Ben Lee <benlee.eelned@gmail.com>
2016-08-18 11:03:54 -0400

    Initial FileReadWriteRunner class established

---
+++ src/tiyinc/noobs/FileReadWriteRunner.java
@@ -0,0 +1,10 @@
+package tiyinc.noobs;
+
+/**
+ * Created by BenLee on 8/18/16.
+ */
+public class FileReadWriteRunner {
+    public static void main(String[] args){
+        System.out.println("Running...");
+    }
+}
