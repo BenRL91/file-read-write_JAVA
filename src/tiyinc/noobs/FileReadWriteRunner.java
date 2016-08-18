b356bde6e165af045ea4af9f208a65f214a9d982
Ben Lee <benlee.eelned@gmail.com>
2016-08-18 11:47:47 -0400

    Add File reader

--- src/tiyinc/noobs/FileReadWriteRunner.java
+++ src/tiyinc/noobs/FileReadWriteRunner.java
@@ -3,6 +3,7 @@ package tiyinc.noobs;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by BenLee on 8/18/16.
@@ -13,10 +14,10 @@ public class FileReadWriteRunner {
         System.out.println("Running...");
         FileReadWriteRunner runner = new FileReadWriteRunner();
         runner.testWriteFile();
+        runner.testReadFile();
     }
 
     public void testWriteFile(){
-        System.out.println("test write file!");
         FileWriter testWriter = null;
         try {
             File testFile = new File("noobs.txt");
@@ -28,10 +29,24 @@ public class FileReadWriteRunner {
             if (testWriter != null ){
                 try {
                     testWriter.close();
-                } catch (Exception e) {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    public void testReadFile(){
+        try {
+            File testFile = new File("src/tiyinc/noobs/FileReadWriteRunner.java");
+            Scanner fileScanner = new Scanner(testFile);
+            while(fileScanner.hasNext()){
+                String currentLine = fileScanner.nextLine();
+                System.out.println(currentLine);
+            }
+        }catch (Exception exceptrion){
+            exceptrion.printStackTrace();
+        }
+
     }
 }
