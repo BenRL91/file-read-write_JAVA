6c9dfbf4455b64f8f5dbf25636c9fdaaf77f3574
Ben Lee <benlee.eelned@gmail.com>
2016-08-18 11:28:30 -0400

    Add FileWrite
    Try and Catch blocks for write and close methods

--- src/tiyinc/noobs/FileReadWriteRunner.java
+++ src/tiyinc/noobs/FileReadWriteRunner.java
@@ -1,10 +1,37 @@
 package tiyinc.noobs;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Created by BenLee on 8/18/16.
  */
+
 public class FileReadWriteRunner {
     public static void main(String[] args){
         System.out.println("Running...");
+        FileReadWriteRunner runner = new FileReadWriteRunner();
+        runner.testWriteFile();
+    }
+
+    public void testWriteFile(){
+        System.out.println("test write file!");
+        FileWriter testWriter = null;
+        try {
+            File testFile = new File("noobs.txt");
+            testWriter = new FileWriter(testFile);
+            testWriter.write("new stuff");
+        }catch (Exception exception) {
+            exception.printStackTrace();
+        }finally {
+            if (testWriter != null ){
+                try {
+                    testWriter.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
